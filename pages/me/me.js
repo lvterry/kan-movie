c3b74a7a16b49616f15f2b12efedfe2e662b232d
Terry Wang <lvterry@gmail.com>
2020-02-23 20:10:23 +0800

    add state to me page when there's no favorited reviews

--- pages/me/me.js
+++ pages/me/me.js
@@ -9,42 +9,7 @@ Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
-    movies: [
-      {
-        id: 2,
-        title: '利刃出鞘',
-        genre: '剧情 / 喜剧 / 悬疑 / 犯罪',
-        poster: 'poster2.jpg',
-        review: {
-          content: '我昨天看完以后在剧情上纠结了很长时间，有很多为什么...',
-          author: {
-            avatar: 'yi.jpg',
-            name: '小小易'
-          }
-        }
-      },
-      {
-        id: 3,
-        title: '追随',
-        genre: '悬疑 / 惊悚 / 犯罪',
-        poster: 'poster3.jpg',
-        review: {
-          content: '随着叙述的深入，一个年轻人孤单的生活展现在观众面前...',
-          author: {
-            avatar: 'jia.jpg',
-            name: '加书亚在路上'
-          }
-        }
-      }
-    ]
-  },
-
-  getUserInfo (event) {
-    app.globalData.userInfo = event.detail.userInfo
-    this.setData({
-      userInfo: event.detail.userInfo,
-      hasUserInfo: true
-    })
+    movies: []
   },
 
   onLoad: function (options) {
@@ -65,5 +30,13 @@ Page({
         }
       }
     })
-  }
+  },
+
+  getUserInfo(event) {
+    app.globalData.userInfo = event.detail.userInfo
+    this.setData({
+      userInfo: event.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
 })
